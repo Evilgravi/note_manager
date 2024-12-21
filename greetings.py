bb7abbc61098cec263f9bd7a2008bdee34105b5a
Evilgravi <temka.202@bk.ru>
2024-12-21 13:31:48 +0300

    Initial commit

---
+++ greetings.py
@@ -0,0 +1,13 @@
+username = "Артём"
+title = "Первая заметка"
+content = "Описание в заметке"
+status = "В работе"
+created_date = "21-12-2024"
+issue_date = "31-12-2024"
+
+print("Имя пользователя: ",username)
+print("Заголовок заметки: ",title)
+print("Описание заметки: ",content)
+print("Статус заметки: ",status)
+print("Дата создания заметки: ",created_date)
+print("Дедлайн: ",issue_date)
