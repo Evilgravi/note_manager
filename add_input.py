7bc382768f256ab2ecb5d667ec90e8339a446414
Evilgravi <temka.202@bk.ru>
2024-12-26 22:22:52 +0300

    25/12/2024 маленькие корректировки

--- add_input.py
+++ add_input.py
@@ -1,8 +1,8 @@
 from datetime import datetime
-
+#ввод пользователем переменных
 username = input("Введите ваше имя: " )
 from add_list import * # ввод заголовков 1 2 3
 content = input("Введите текст заметки: ")
 status = input("Введите статус задачи: ")
-created_date = datetime.strptime(input("Введите дату начала задачи. Пример: (2024-12-22):"),"%Y-%m-%d")
-issue_date = datetime.strptime(input("Введите дату окончания задачи. Пример: (2024-12-22):"),"%Y-%m-%d")
+created_date = datetime.strptime(input("Введите дату начала задачи. Пример: (2024-12-22): "),"%Y-%m-%d")
+issue_date = datetime.strptime(input("Введите дату окончания задачи. Пример: (2024-12-22): "),"%Y-%m-%d")
--- date_changer.py
+++ date_changer.py
@@ -1,4 +1,4 @@
 from add_input import * # все переменные, тут формируются даты
-
+# преобразование дат
 temp_created_date = created_date.strftime("%d-%m")
 temp_issue_date = issue_date.strftime("%d-%m")
